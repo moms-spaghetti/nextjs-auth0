0c4ef3db91cc214849fa77967550c108b5df8eb6
moms-spaghetti <williamedwards36@aol.com>
2020-12-18 17:57:25 +0000

    third commit

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,8 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>Testing NextJS Auth0</h1>
-      <h2>Another Line</h2>
-      <h2>Third line</h2>
+      <h1>Login Testing NextJS Auth0</h1>
+      
     </div>
   )
 }
